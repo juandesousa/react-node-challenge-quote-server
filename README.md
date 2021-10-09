20e08242ec24f53194dde509852ce8933f9a4b4f
juandesousa <juan.cdsb@gmail.com>
2021-10-09 13:34:33 +0200

    Extra Homework

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 In the project directory, you can run:
 
+### `API`
+
+Run the API https://github.com/juandesousa/node-challenge-quote-server
+
 ### `npm start`
 
 Runs the app in the development mode.\
--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [searchRandom, setSearchRandom] = useState();
+  const [searchTerm, setSearchTerm] = useState();
+
+  useEffect(() => { getRandom() }
+  ,[])
+
+  const getRandom = () => {
+    fetch("http://localhost:5000/quotes/random")
+      .then(res => res.json())
+      .then(data => setSearchRandom(data));
+  }
+
+  const getTerm = () => {
+    console.log(searchTerm)
+    fetch(`http://localhost:5000/quotes/search?term=${searchTerm}`)
+    .then(res => res.json())
+    .then(data => data.length>0 ? setSearchRandom(data[0]) : setSearchRandom({"quote":"No Results", "author":""}));
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="input-group p-5">
+      <input type="text" className="form-control m-5" placeholder="Search Term" onChange={(event)=>setSearchTerm(event.target.value)}/>
+      <button className="btn btn-outline-secondary m-5" type="button" onClick={getTerm}>Search</button>
+      <button className="btn btn-outline-secondary m-5" type="button" onClick={getRandom}>Random</button>
     </div>
+    {searchRandom ? <div className="p-5"><p className="card-text d-flex justify-content-center fs-1">{searchRandom.quote}</p><h5 className="card-title d-flex justify-content-end mx-5 px-5">{searchRandom.author}</h5></div> 
+    : null}
+    </>
   );
 }
 
 export default App;
+
+
